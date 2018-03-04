c1f4c78c61e622abb0d513a62d74f304e96c2329
Olivier André <olivier.andre77@gmail.com>
2018-03-04 14:43:47 +0100

    Remove htaccess. Improve app.vue

--- assets/js/vue/App.vue
+++ assets/js/vue/App.vue
@@ -1,32 +1,42 @@
 <template>
 <div>
-  <div>
+  <div v-if="show">
     <input v-model="firstname" placeholder="edit me">
   </div>
   <div v-if="firstname">
     <div>
-      <p>Message is: {{ firstname }}</p>
+      <p>Your firstname is: {{ firstname }}</p>
     </div>
     <button v-on:click="counter += 1">Add 1</button>
     <p>The button above has been clicked {{ counter }} times.</p>
     <button v-on:click="say('Hello')">Say "Hello {{ firstname }}"</button>
+  </div>
+  <div v-else>
+    <p class="waiting">You must wait 2 seconds</p>
   </div>
 </div>
 </template>
 
 <script>
 export default {
-  name: "vue",
   data() {
     return {
       firstname: null,
       counter: 0,
-      show: true
+      show: false
     };
   },
   methods: {
     say(name) {
       alert(name + ' ' + this.firstname);
+    },
+    addPoint() {
+      var waiting = document.querySelector('.waiting');
+      waiting.innerText += '.';
+    },
+    updateFirstname(firstname) {
+      this.firstname = firstname;
+      this.show = true;
     }
   },
   created() {
@@ -34,8 +44,10 @@ export default {
       .then((response) => response.json())
       .then(function(data) {
         var self = this;
+        var interval = setInterval(self.addPoint, 500);
         setTimeout(function() {
-          self.firstname = data.firstname;
+          self.updateFirstname(data.firstname)
+          clearInterval(interval);
         }, 2000);
       }.bind(this));
   }
